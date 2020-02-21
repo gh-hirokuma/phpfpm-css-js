eea68a90d804529559e8ffedd9a58318a20acac9
Hiroshige Negishi <hirokuma@globhcet.org>
2020-02-21 22:16:48 +0700

    initial commit

---
+++ code/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c&display=swap" rel="stylesheet"> 
+  <link rel="stylesheet" href="./assets/css/main.css" />
+</head>
+<body>
+  <h1 class="header">
+    <?php echo "こんつわ PHP"; ?>
+  </h1>
+
+  <script
+    src="https://code.jquery.com/jquery-3.4.1.min.js"
+    integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+    crossorigin="anonymous"></script>
+    <script src="./assets/js/main.js"></script>
+</body>
+</html>
